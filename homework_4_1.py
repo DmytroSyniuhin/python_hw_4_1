3fbbbc4e9ac92c62a2f033ea9b7fc4c8d96d4304
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2024-01-29 00:36:24 +0200

    homework

---
+++ homework_4_1.py
@@ -0,0 +1,46 @@
+list_1 = [0, 1, 0, 12, 3]
+count_1 = 1
+number_1 = list_1.count(0)
+
+while count_1 <= number_1:
+    list_1.append(0)
+    b = list_1.index(0)
+    list_1.pop(b)
+    count_1 += 1
+print(list_1)
+
+
+list_2 = [0]
+count_2 = 1
+number_2 = list_2.count(0)
+
+while count_2 <= number_2:
+    list_2.append(0)
+    b = list_2.index(0)
+    list_2.pop(b)
+    count_2 += 1
+print(list_2)
+
+
+list_3 = [1, 0, 13, 0, 0, 0, 5]
+count_3 = 1
+number_3 = list_3.count(0)
+
+while count_3 <= number_3:
+    list_3.append(0)
+    b = list_3.index(0)
+    list_3.pop(b)
+    count_3 += 1
+print(list_3)
+
+
+list_4 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+count_4 = 1
+number_4 = list_4.count(0)
+
+while count_4 <= number_4:
+    list_4.append(0)
+    b = list_4.index(0)
+    list_4.pop(b)
+    count_4 += 1
+print(list_4)
